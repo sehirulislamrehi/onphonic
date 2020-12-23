59b7c923c2ca050a7cab7996c310b52db6421689
Sehirul Islam Rehi <mdsehirulislamrehi@gmail.com>
2020-12-23 15:46:02 +0600

    home page done

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,144 @@
+//window scroll desktop
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if( $(window).scrollTop() > 100 ){
+            $(".top-logo").css({
+                "position" : "fixed",
+                "top" : "0",
+                "left" : "0",
+                "width" : "100%",
+            })
+        }
+        else{
+            $(".top-logo").css({
+                "position" : "unset",
+                "top" : "0",
+                "left" : "0",
+                "width" : "100%",
+            })
+        }
+    })
+})
+
+
+
+//window scroll mobile
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if( $(window).scrollTop() > 20 ){
+            $(".search-mob").css({
+                "position" : "fixed",
+                "top" : "0",
+                "left" : "0",
+                "width" : "100%",
+            })
+        }
+        else{
+            $(".search-mob").css({
+                "position" : "unset",
+                "top" : "0",
+                "left" : "0",
+                "width" : "100%",
+            })
+        }
+    })
+})
+
+
+
+
+
+
+
+
+//product carousel
+$('.product-carousel').owlCarousel({
+    loop:false,
+    nav:true,
+    dots: false,
+    autoplay:false,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+
+
+//client carousel
+$('.client-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+//ad carousel
+$('.ad-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+//testimonial carousel
+$('.testimonial-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 
 
 //banner carousel
